4cb9015fde49221ee50ad48d44e62ff73c8c1373
Rey-Diaz <Reinoldiaz@gmail.com>
2023-10-31 15:54:00 -0400

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
 // src/components/Header.jsx
 
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    return JSON.parse(localStorage.getItem('darkMode')) || false;
+  });
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -26,6 +43,9 @@ const Header = () => {
             </Link>
             {/* Add more navigation links as needed */}
           </nav>
+          <IconButton onClick={handleDarkModeToggle} color="inherit">
+            {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
           <Button color="inherit" component={Link} to="/login">
             Login
           </Button>
